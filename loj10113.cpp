90819cda886690217cf56009258f0e0ab85d1951
wky32768 <527192083@qq.com>
2019-02-14 15:49:07 +0800

    Create loj10113.cpp

---
+++ loj10113.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define int long long
+#define lowbit(x) (x&(-x))
+using namespace std;
+int c[1200000],n,m,opt,x,y;
+void change(int at,int x) {
+	for(int i=at;i<=n;i+=lowbit(i)) c[i]+=x;
+}
+int tot(int at) {
+	int ans=0;
+	for(int i=at;i;i-=lowbit(i)) ans+=c[i];
+	return ans;
+}
+signed main() {
+	cin>>n>>m;
+	for(int i=1;i<=n;i++) {
+		cin>>x;
+		change(i,x);
+	}
+	while(m--) {
+		cin>>opt>>x>>y;
+		if(opt==1) {
+			change(x,y);
+		} else cout<<tot(y)-tot(x-1)<<'\n';
+	}
+ 	return 0;
+}
